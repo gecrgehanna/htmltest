b5e5717f7c26fa6da7eddf5a5f3e04feaa908a1f
gecrgehanna <georges.hanna@students.iaac.net>
2021-10-23 13:45:34 +0200

    first website restructuring

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
-dragElement(document.getElementById("dragbootcamp"));
+dragElement(document.getElementById("dragbootcamp" ));
+dragElement(document.getElementById("dragaows"));
+dragElement(document.getElementById("dragbio"));
 
 function dragElement(elmnt) {
-  var pos1 = 5 , pos2 = 5, pos3 = 0, pos4 = 0;
+  var pos1 = 0 , pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
@@ -40,3 +42,4 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
