f3c0f1adb944ae24827a9ebe234a50932727b79f
gecrgehanna <georges.hanna@students.iaac.net>
2021-11-01 00:03:34 +0100

    placement of boxes

--- pages/javascript.js
+++ pages/javascript.js
@@ -1,5 +1,6 @@
 
 dragElement(document.getElementById("dragbio"));
+dragElement(document.getElementById("dragbio1"));
 
 function dragElement(elmnt) {
   var pos1 = 0 , pos2 = 0, pos3 = 0, pos4 = 0;
